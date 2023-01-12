66e980a771c631a3b6340c9a2eae00c1d32d7823
Any Medola <anynhamedola@gmail.com>
2023-01-12 20:16:07 -0300

    mainscreen

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: Router
+  ) { }
 
   ngOnInit() {
   }
+
+  homePage(){
+    this.route.navigate(['home']) 
+   }
+
+   forgotPage(){
+    this.route.navigate(['forgotpassword']) 
+   }
+
+   signUpPage(){
+    this.route.navigate(['signup']) 
+   }
+
+   loginPage(){
+    this.route.navigate(['login']) 
+   }
 
 }
